756f665db8a681aabe9b0cb787f5647ae2bed93d
HokageMinato <dsparda996@gmail.com>
2023-10-28 15:52:35 +0530

    examples updated

--- Assets/Scenes/Scripts/ScriptableEnumDemo.cs
+++ Assets/Scenes/Scripts/ScriptableEnumDemo.cs
@@ -7,17 +7,28 @@ using System;
 public class ScriptableEnumDemo : MonoBehaviour
 {
     //Declare like normal enum, The values will be drawn by the values set in scriptable object.
-    public ScriptableEnum playerType;
+    
+    public ScriptableEnum playerType; //Apply menu filter by setting inspector to 'Debug Mode' 
+                                      //That will allow for filtered navigation.
+
     public ScriptableEnum playerType2;
+
     
     public ScriptableEnum enemyType;
 
-    //ScriptableEnumsContainer for iterating all values.
-    public BaseScriptableEnumValueContainer playerTypes;
+                                                          //ScriptableEnumsContainer for iterating all values.
+    public BaseScriptableEnumValueContainer playerTypes; //Any asset can be a container type and
+                                                        //values can be fed from them.
 
+    public string tankTypeStringValue;
+    public ScriptableEnum tankTypeFromStringValue;
+
+
+    public List<ScriptableEnum> instantiatedEnemyInstances = new List<ScriptableEnum>();
 
     private void Start()
     {
+
         //xyzScEnum.String to utilize the string representation.
         Debug.Log($"{nameof(playerType)} : {playerType.StringId}"); 
         Debug.Log($"{nameof(enemyType)} : {enemyType.StringId}");
@@ -42,6 +53,18 @@ public class ScriptableEnumDemo : MonoBehaviour
         Debug.Log($"{nameof(playerType)} == {nameof(playerType2)} : Are Same ? {playerType != playerType2}");
 
 
+        //Generate ScriptableEnum from pureStrings,
+        //Will ensure same hash is assigned to this instance.
+        tankTypeFromStringValue = new ScriptableEnum(tankTypeStringValue);
+
+        //Also provides ability to Generate 'RuntimeId' with equal performance
+        //These values should be unique, and will reflect at 'RuntimeGenerated' scriptable container.
+        //NOTE: Never save FastID as prefs or any mode of persistant storage.
+        //      Any reordering/addition/removal in container will lead to variance in fastId.
+        instantiatedEnemyInstances.Add(ScriptableEnum.InstantiateRuntimeInstance(enemyType.StringId + "_1"));
+        instantiatedEnemyInstances.Add(ScriptableEnum.InstantiateRuntimeInstance(enemyType.StringId + "_2"));
+        instantiatedEnemyInstances.Add(ScriptableEnum.InstantiateRuntimeInstance(enemyType.StringId + "_3"));
+
         //Can iterate enum container to access all values, for usecases like typeof().GetEnumNames().
         // PS looking for a better workflow where enum instance can directly acces those values.
         string values ="Player Types:";
@@ -49,6 +72,11 @@ public class ScriptableEnumDemo : MonoBehaviour
         Debug.Log(values);
         
 
+    }
+
+    private void OnDisable()
+    {
+        instantiatedEnemyInstances.Clear();
     }
 
 
--- Assets/ScriptableEnum/RuntimeCore/ScriptableEnumValueContainer.cs
+++ Assets/ScriptableEnum/RuntimeCore/ScriptableEnumValueContainer.cs
@@ -6,7 +6,6 @@ namespace ScriptableEnumSystem
     [CreateAssetMenu(fileName = "RuntimeIdContainer", menuName = "Scriptable Objects/ScriptableEnum/RuntimeIdContainer")]
     public class BaseScriptableEnumValueContainer : ScriptableObject
     {
-
         [SerializeField] protected List<string> _ids;
 
         public virtual List<string> Ids  {   get { return _ids; }  }
